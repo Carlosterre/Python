ea19cd007ad7e59075d36342e358254772373225
Carlosterre <68872405+Carlosterre@users.noreply.github.com>
2020-07-30 21:09:54 +0200

    Add files via upload

---
+++ Sumatorio.py
@@ -0,0 +1,7 @@
+# SUMATORIO DE LOS N PRIMEROS ENTEROS POSITIVOS
+
+n = int(input("n: "))
+
+sm = n * (n + 1) / 2                                                           # Sumatorio
+
+print("La suma de los", n, "primeros enteros positivos es", sm)
