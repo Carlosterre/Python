2e8fb55a15089b7b59e69ae665cd38e845497556
Carlosterre <68872405+Carlosterre@users.noreply.github.com>
2020-08-01 13:20:23 +0200

    Delete Area_habitacion.py

--- Area_habitacion.py
+++
@@ -1,9 +0,0 @@
-# AREA DE UNA HABITACION
-# Calcula el area de una habitacion rectangular
-
-length = float(input("Longitud de la habitacion (m): "))
-width = float(input("Anchura de la habitacion (m): "))
-
-area = length * width
-
-print("El area es", area, "m")
